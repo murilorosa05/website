137661b2461c1a20de15c037e3eb6229f85a746e
Murilo da Silva Rosa <murilosilvarosa188@gmail.com>
2025-08-08 13:12:50 -0300

    b

--- app/components/ClickableImage.tsx
+++ app/components/ClickableImage.tsx
@@ -31,7 +31,7 @@ export function ClickableImage() {
         priority
       />
       {copied && (
-        <div className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center text-white text-sm font-medium">
+        <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center text-white text-sm font-medium">
           Chave PIX copiada!
         </div>
       )}
