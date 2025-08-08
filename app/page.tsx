f1304ef2f6812b7d58fe8c566d4aaa3a1a087bda
Murilo da Silva Rosa <murilosilvarosa188@gmail.com>
2025-08-08 03:19:56 -0300

    c

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Page() {
       <p className="mb-4">
         {`Opa.`}
       </p>
-      <div className="my-8">
+{/*      <div className="my-8">
         <BlogPosts />
-      </div>
+      </div> */}
     </section>
   )
 }
