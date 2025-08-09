9743c060153920e991b04f968349873d69f8573a
Murilo da Silva Rosa <murilosilvarosa188@gmail.com>
2025-08-09 08:50:56 -0300

    update

---
+++ app/nathan/layout.tsx
@@ -0,0 +1,10 @@
+// app/nathan/layout.tsx
+
+export const metadata = {
+  title: 'Nathan',
+  description: 'Essa é a página do Nathan.',
+}
+
+export default function NathanLayout({ children }) {
+  return <>{children}</>
+}
